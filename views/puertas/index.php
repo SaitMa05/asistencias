84007f649f92f31df85cac149b1b892caffd4bc7
SaitMa099 <bbvgffcx@gmail.com>
2024-10-02 12:10:44 -0300

    Puertas CSS

---
+++ views/puertas/index.php
@@ -0,0 +1,52 @@
+<main>
+    <div class="puertas">
+        <div class="d-grid gap-2">
+            <div class="d-grid puerta mb-2">
+                <button type="button" class="btn btn-puertas p-3">Laboratoria III</button>
+                <div class="historial ">
+                    <li class="bg-dark text-white p-2 m-0">Matias Morelli 19/06/2024 00:00</li>
+                    <li class="bg-dark text-white p-2 m-0">Matias Morelli 19/06/2024 00:00</li>
+                    <li class="bg-dark text-white p-2 m-0">Matias Morelli 19/06/2024 00:00</li>
+                </div>
+            </div>
+
+            <div class="d-grid puerta mb-2">
+                <button type="button" class="btn btn-puertas p-3">Laboratorio II</button>
+                <div class="historial">
+                    <li class="bg-dark text-white p-2 m-0">Messi 10 19/06/2024 00:00</li>
+                    <li class="bg-dark text-white p-2 m-0">Matias Morelli 19/06/2024 00:00</li>
+                    <li class="bg-dark text-white p-2 m-0">Matias Morelli 19/06/2024 00:00</li>
+                </div>
+            </div>
+
+            <div class="d-grid puerta mb-2">
+                <button type="button" class="btn btn-puertas p-3">Laboratorio I</button>
+                <div class="historial">
+                    <li class="bg-dark text-white p-2 m-0">Matias Morelli 19/06/2024 00:00</li>
+                    <li class="bg-dark text-white p-2 m-0">Matias Morelli 19/06/2024 00:00</li>
+                    <li class="bg-dark text-white p-2 m-0">Matias Morelli 19/06/2024 00:00</li>
+                </div>
+            </div>
+            
+            <div class="d-grid puerta mb-2">
+                <button type="button" class="btn btn-puertas p-3">Meya</button>
+                <div class="historial">
+                    <li class="bg-dark text-white p-2 m-0">Matias Morelli 19/06/2024 00:00</li>
+                    <li class="bg-dark text-white p-2 m-0">Matias Morelli 19/06/2024 00:00</li>
+                    <li class="bg-dark text-white p-2 m-0">Matias Morelli 19/06/2024 00:00</li>
+                </div>
+            </div>
+
+            <div class="d-grid puerta mb-2">
+                <button type="button" class="btn btn-puertas p-3">APE</button>
+                <div class="historial">
+                    <li class="bg-dark text-white p-2 m-0">Matias Morelli 19/06/2024 00:00</li>
+                    <li class="bg-dark text-white p-2 m-0">Matias Morelli 19/06/2024 00:00</li>
+                    <li class="bg-dark text-white p-2 m-0">Matias Morelli 19/06/2024 00:00</li>
+                </div>
+            </div>
+            
+            
+        </div>
+    </div>
+</main>
